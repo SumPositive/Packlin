80bab5373d24fa15508273fcd8399d8ae6b1471d
sumpo <52391405+SumPositive@users.noreply.github.com>
2025-09-05 14:06:14 +0900

    Rename models and enhance lists

--- PackList/PackListApp.swift
+++ PackList/PackListApp.swift
@@ -12,21 +12,21 @@ import SwiftData
 struct PackListApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Title.self,
-            Group.self,
-            Item.self,
+            E1Title.self,
+            E2Group.self,
+            E3Item.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
         do {
             let container = try ModelContainer(for: schema, configurations: [modelConfiguration])
 //            let context = container.mainContext
-//            let descriptor = FetchDescriptor<Title>()
+//            let descriptor = FetchDescriptor<E1Title>()
 //            let existing = try context.fetch(descriptor)
 //            if existing.isEmpty {
-//                let title = Title(name: "New Title", note: "新しいPackListのタイトルを追加する")
-//                let group = Group(name: "New Group", parent: title)
-//                _ = Item(name: "New Item", parent: group)
+//                let title = E1Title(name: "New Title", note: "新しいPackListのタイトルを追加する")
+//                let group = E2Group(name: "New Group", parent: title)
+//                _ = E3Item(name: "New Item", parent: group)
 //                context.insert(title)
 //                try context.save()
 //            }
