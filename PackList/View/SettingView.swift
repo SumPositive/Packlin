6961ed405370662ddda6f38d084ee661e3e1625f
sumpo <sumpo@azukid.com>
2025-09-21 11:10:23 +0900

    no message

--- PackList/View/SettingView.swift
+++ PackList/View/SettingView.swift
@@ -12,11 +12,6 @@ import AVKit
 /// 設定画面：Popupで表示する
 struct SettingView: View {
     
-    @State private var showSafari = false
-    @State private var showAd = false
-    @State private var showAdMovie = false
-    @State private var showDonate = false
-
     var body: some View {
         VStack(spacing: 0) {
             HStack {
@@ -26,106 +21,16 @@ struct SettingView: View {
             }
             .padding(8)
             
-            HStack {
-                // 情報（ボタン）
-                Button(action: {
-                    withAnimation {
-                        // SafariでURLを表示する
-                        showSafari = true
-                    }
-                }) {
-                    Image(systemName: "info.circle")
-                    Text("アプリの紹介・取扱説明")
-                }
-                .contentShape(Rectangle()) // paddingを含む領域全体をタップ対象にする
-                .sheet(isPresented: $showSafari) {
-                    let urlString = String(localized: "info.url")
-                    if let url = URL(string: urlString) {
-                        SafariView(url: url)
-                    } else {
-                        Text("setting.infoUnavailable")
-                    }
-                }
-                
-                Spacer()
-            }
-            .padding(8)
+            InformationView()
+                .padding(.vertical, 8)
 
-            HStack {
-                // 広告を見て寄付する（ボタン）
-                Button(action: {
-                    withAnimation {
-                        // SafariでURLを表示する
-                        showAd = true
-                    }
-                }) {
-                    Image(systemName: "heart.fill")
-                    Text("広告を見て寄付")
-                }
-                .contentShape(Rectangle()) // paddingを含む領域全体をタップ対象にする
-                .sheet(isPresented: $showAd) {
-                    //TODO:広告バナーを3種類を同時に表示する
-                }
-                Spacer()
-            }
-            .padding(8)
-
-            HStack {
-                // 動画広告を見て寄付する（ボタン）
-                Button(action: {
-                    withAnimation {
-                        // SafariでURLを表示する
-                        showAdMovie = true
-                    }
-                }) {
-                    Image(systemName: "heart.fill")
-                    Text("動画広告を見て寄付")
-                }
-                .contentShape(Rectangle()) // paddingを含む領域全体をタップ対象にする
-                .sheet(isPresented: $showAdMovie) {
-                    VideoAdContainerView()
-                }
-                HStack(spacing: 0) {
-                    Image(systemName: "exclamationmark.triangle")
-                        .imageScale(.small)
-                    Text("音が出ます").font(.caption)
-                }
-                Spacer()
-            }
-            .padding(8)
-//            Text("無料WiFiに繋いでいる時にでもよろしくお願いします")
-//                .font(.caption2)
-//                .padding(.leading, 20)
-//                .padding(.top, 2)
-            
-            HStack {
-                // **＊送金て寄付する（ボタン）
-                Button(action: {
-                    withAnimation {
-                        // SafariでURLを表示する
-                        showDonate = true
-                    }
-                }) {
-                    Image(systemName: "heart.fill")
-                    Text("ことら送金で寄付")
-                }
-                .contentShape(Rectangle()) // paddingを含む領域全体をタップ対象にする
-                .sheet(isPresented: $showDonate) {
-                    //TODO:ことら送金で寄付する
-                }
-//                HStack(spacing: 0) {
-//                    Image(systemName: "exclamationmark.triangle")
-//                        .imageScale(.small)
-//                    Text("音が出ます").font(.caption)
-//                }
-                Spacer()
-            }
-            .padding(8)
+            DonationView()
+                .padding(.vertical, 8)
 
             Spacer()
         }
         .padding(.horizontal, 8)
-        .frame(width: 300, height: 300)
+        .frame(width: 300, height: 340)
         .onAppear {
 
         }
@@ -141,6 +46,117 @@ struct SettingView: View {
             return SFSafariViewController(url: url)
         }
         func updateUIViewController(_ uiViewController: SFSafariViewController, context: Context) {}
+    }
+
+    /// アプリの紹介・取扱説明
+    struct InformationView: View {
+        @State private var showSafari = false
+        
+        var body: some View {
+            Button(action: {
+                //withAnimation {
+                    // SafariでURLを表示する
+                    showSafari = true
+                //}
+            }) {
+                Image(systemName: "info.circle")
+                Text("アプリの紹介・取扱説明")
+                Spacer()
+            }
+            .padding(8)
+            .sheet(isPresented: $showSafari) {
+                let urlString = String(localized: "info.url")
+                if let url = URL(string: urlString) {
+                    SafariView(url: url)
+                } else {
+                    Text("setting.infoUnavailable")
+                }
+            }
+        }
+    }
+
+    /// 寄付
+    struct DonationView: View {
+        @State private var showAd = false
+        @State private var showAdMovie = false
+        @State private var showDonate = false
+
+        var body: some View {
+            VStack {
+                HStack {
+                    Image(systemName: "heart")
+                    Text("開発者を支援する")
+                    Spacer()
+                }
+                .padding(8)
+                VStack {
+                    HStack {
+                        // 広告を見て寄付する（ボタン）
+                        Button(action: {
+                            withAnimation {
+                                // SafariでURLを表示する
+                                showAd = true
+                            }
+                        }) {
+                            Text("広告を見て寄付")
+                        }
+                        .contentShape(Rectangle()) // paddingを含む領域全体をタップ対象にする
+                        .sheet(isPresented: $showAd) {
+                            //TODO:収益広告バナーを3種類を同時に表示する
+                        }
+                        Spacer()
+                    }
+                    .padding(.horizontal, 8)
+                    .padding(.bottom, 8)
+                    
+                    HStack {
+                        // 動画広告を見て寄付する（ボタン）
+                        Button(action: {
+                            withAnimation {
+                                // SafariでURLを表示する
+                                showAdMovie = true
+                            }
+                        }) {
+                            Text("動画広告を見て寄付")
+                        }
+                        .contentShape(Rectangle()) // paddingを含む領域全体をタップ対象にする
+                        .sheet(isPresented: $showAdMovie) {
+                            VideoAdContainerView()
+                        }
+                        HStack(spacing: 0) {
+                            Image(systemName: "exclamationmark.triangle")
+                                .imageScale(.small)
+                            Text("音が出ます").font(.caption)
+                        }
+                        Spacer()
+                    }
+                    .padding(.horizontal, 8)
+                    .padding(.bottom, 8)
+                    
+                    HStack {
+                        // **＊送金て寄付する（ボタン）
+                        Button(action: {
+                            withAnimation {
+                                // SafariでURLを表示する
+                                showDonate = true
+                            }
+                        }) {
+                            Text("ことら送金で寄付")
+                        }
+                        .contentShape(Rectangle()) // paddingを含む領域全体をタップ対象にする
+                        .sheet(isPresented: $showDonate) {
+                            //TODO:ことら送金で寄付する
+                        }
+                        Spacer()
+                    }
+                    .padding(.horizontal, 8)
+                    .padding(.bottom, 8)
+                }
+                .padding(.leading, 26)
+            }
+            .background(Color(.white).opacity(0.5))
+            .cornerRadius(12)
+        }
     }
 
     /// 広告動画の表示を管理するビュー
